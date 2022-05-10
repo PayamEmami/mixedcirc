c3e41ca16d263d5eb5adf646814aa9491d616b97
Marcin Kierczak <marcin.kierczak@scilifelab.se>
2022-05-10 09:53:26 +0200

    fix typo in input checks

--- R/mixedcirc_adjust.R
+++ R/mixedcirc_adjust.R
@@ -34,55 +34,64 @@
 #' @import variancePartition
 #' @import multtest
 
+mixedcirc_adjust <- function(input, method="BH", pattern="p_value", verbose=FALSE, ...) {
 
-
-mixedcirc_adjust <- function(input,method="BH",pattern="p_value",verbose=FALSE,...){
-
-
-  if(verbose)cat("Checking inputs ...\n")
+  if (verbose) {
+    cat("Checking inputs ...\n")
+  }
   # checking inputs
-  if(is(input,"mixedcirc_fit_list."))
+  if (is(input,"mixedcirc_fit_list.")) {
     stop("input must be a data frame or matrix")
-
+  }
 
   if(!method%in%c("holm", "hochberg", "hommel", "bonferroni", "BH", "BY",
-                  "fdr", "none"))
+                  "fdr", "none")) {
     stop("method must be one of Bonferroni, holm, hochberg, hommel, bonferroni, BH, BY,
                   fdr, none")
+  }
 
-  if(!is.character(pattern))
+  if(!is.character(pattern)) {
     stop("pattern must be a string (character)")
+  }
 
 
+  if(verbose) {
+    cat("gathering the data ...\n")
+  }
 
-  if(verbose)cat("gathering the data ...\n")
   data_gathered <- input@results
-  data_gathered <- do.call(rbind,lapply(data_gathered,function(x){x@results}))
+  data_gathered <- do.call(rbind,lapply(data_gathered,function(x){ x@results }))
 
   selected_pvalues <- grepl(pattern = pattern,x = colnames(data_gathered))
 
-  if(sum(selected_pvalues)==0)
+  if(sum(selected_pvalues)==0) {
     stop("Did not find any column matching the provided pattern")
+  }
 
-  selected_data<-data_gathered[,selected_pvalues,drop=F]
+  selected_data<-data_gathered[ ,selected_pvalues,drop=F]
 
-  if(verbose)cat("Performing adjustment ...\n")
+  if (verbose) {
+    cat("Performing adjustment ...\n")
+  }
+
   adjusted_pvalues<-apply(selected_data,MARGIN = 2,function(x){
     matrix(p.adjust(p = x,method = method))
   })[,,drop=F]
 
   colnames(adjusted_pvalues)<-paste(colnames(selected_data),"_adjusted_",method,sep="")
 
-  if(verbose)cat("Preparing the results ...\n")
+  if (verbose) {
+    cat("Preparing the results ...\n")
+  }
 
-  for(i in 1:length(input))
-  {
+  for (i in 1:length(input)) {
     input@results[[i]]@results<-cbind(input@results[[i]]@results[,,drop=F],adjusted_pvalues[i,,drop=F])
   }
 
-  if(verbose)cat("Finished\n")
-
-    return((input))
-
+  if(verbose) {
+    cat("Finished\n")
   }
+
+return((input))
+}
 
