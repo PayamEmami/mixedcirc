9e0681fb9f35d6e3e386949c70e420143ad39e5d
Marcin Kierczak <marcin.kierczak@scilifelab.se>
2022-05-10 10:14:32 +0200

    make error messages more coherent

--- R/mixedcirc_adjust.R
+++ R/mixedcirc_adjust.R
@@ -11,9 +11,9 @@
 #' @examples
 #' data("circa_data")
 #'
-#'results<-mixedcirc_detect(data_input = circa_data$data_matrix,
-#'time = circa_data$time,group = circa_data$group,id = circa_data$id,period = 24,verbose = TRUE)
-#'results<-mixedcirc_adjust(results,method="Bonferroni")
+#'results <- mixedcirc_detect(data_input = circa_data$data_matrix,
+#'time = circa_data$time, group = circa_data$group, id = circa_data$id, period = 24, verbose = TRUE)
+#'results <- mixedcirc_adjust(results, method="Bonferroni")
 #'
 #' @return
 #' A class of mixedcirc_fit_list in which the adjusted p-values have been added in the results
@@ -43,44 +43,44 @@ mixedcirc_adjust <- function(input, method="BH", pattern="p_value", verbose=FALS
   # checking inputs
   if (!is(input, "mixedcirc_fit_list")) {
     #stop("input must be a data frame or matrix")
-    stop("ERROR! Input must be a mixedcirc_fit_list object!")
+    stop("input must be a mixedcirc_fit_list object!")
   }
 
   if (length(input) < 2) {
-    stop("ERROR! Input has to be at least 2 elements long!")
+    stop("input has to be at least 2 elements long!")
   }
 
   if(!method %in% c("holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none")) {
-    stop("ERROR! Method must be one of: 'holm', 'hochberg', 'hommel', 'bonferroni', 'BH', 'BY', 'fdr' or 'none'")
+    stop("method must be one of: 'holm', 'hochberg', 'hommel', 'bonferroni', 'BH', 'BY', 'fdr' or 'none'!")
   }
 
   if(!is.character(pattern)) {
-    stop("ERROR! Pattern must be a string (character vector)!")
+    stop("pattern must be a string (character vector)!")
   }
 
 
-  if(verbose) {
+  if (verbose) {
     cat("Gathering the data ...\n")
   }
 
   data_gathered <- input@results
-  data_gathered <- do.call(rbind,lapply(data_gathered,function(x){ x@results }))
+  data_gathered <- do.call(rbind,lapply(data_gathered, function(x) { x@results }))
 
   selected_pvalues <- grepl(pattern = pattern,x = colnames(data_gathered))
 
-  if(sum(selected_pvalues)==0) {
-    stop("Did not find any column matching the provided pattern")
+  if (sum(selected_pvalues) == 0) {
+    stop("did not find any column matching the provided pattern!")
   }
 
-  selected_data<-data_gathered[ ,selected_pvalues,drop=F]
+  selected_data<-data_gathered[ , selected_pvalues, drop=F]
 
   if (verbose) {
     cat("Performing adjustment ...\n")
   }
 
-  adjusted_pvalues<-apply(selected_data,MARGIN = 2,function(x){
-    matrix(p.adjust(p = x,method = method))
-  })[,,drop=F]
+  adjusted_pvalues <- apply(selected_data, MARGIN = 2, function(x) {
+    matrix(p.adjust(p = x, method = method))
+  }) [, , drop=F]
 
   colnames(adjusted_pvalues)<-paste(colnames(selected_data),"_adjusted_",method,sep="")
 
@@ -92,10 +92,9 @@ mixedcirc_adjust <- function(input, method="BH", pattern="p_value", verbose=FALS
     input@results[[i]]@results<-cbind(input@results[[i]]@results[,,drop=F],adjusted_pvalues[i,,drop=F])
   }
 
-  if(verbose) {
+  if (verbose) {
     cat("Finished\n")
   }
 
 return((input))
 }
-
